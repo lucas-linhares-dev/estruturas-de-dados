fb9f3ae08c4ba6a95857b479ff5b13cd9334cb4d
Lucas <lucas_linhares_@hotmail.com>
2022-10-19 17:23:23 -0300

    Implementando metodos ordenar()

--- src/Fila.java
+++ src/Fila.java
@@ -19,6 +19,13 @@ public class Fila<T> {
 		return (Double) this.lista.getPrimeiro().getValor();
 	}
 	
+	public boolean isEmpty() {
+		if(this.lista.getTamanho() <= 0) {
+			return true;
+		}
+		return false;
+	}
+	
 	public Fila<T> clonar() {
 		Fila novaFila = new Fila<T>();
 		for(int i = 0 ; i < lista.getTamanho() ; i++) {
@@ -30,6 +37,29 @@ public class Fila<T> {
 	}
 	
 	public void inverter() {
+		
+	}
+	
+	public void ordenar() {
+		Fila filaAux = new Fila<T>();
+		Fila filaAux2 = new Fila<T>();
+		double menor = this.get();
+		while(!this.isEmpty()) { // Achar o menor
+			double elemento = this.get();
+			if(elemento < menor) {
+				menor = elemento;
+			}
+			filaAux.adicionar(this.get());
+			this.remover();
+		}
+		
+		
+		while(!filaAux.isEmpty()) { // Devolvi e zerei a auxiliar
+			this.adicionar(filaAux.get());
+			filaAux.remover();
+		}
+		
+		
 		
 	}
 }
